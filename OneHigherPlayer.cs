33645c65b2c3e155967a9cef82c0c09646618ec0
Jackson <jacksonrgoodman@gmail.com>
2021-06-08 09:12:36 -0500

    initial commit

---
+++ OneHigherPlayer.cs
@@ -0,0 +1,10 @@
+namespace ShootingDice
+{
+    // TODO: Complete this class
+
+    // Override the Play method to make a Player who always roles one higher than the other player
+    public class OneHigherPlayer
+    {
+
+    }
+}
---
+++ SoreLoserUpperHalfPlayer.cs
@@ -0,0 +1,11 @@
+namespace ShootingDice
+{
+    // TODO: Complete this class
+
+    // A Player who always rolls in the upper half of their possible role and
+    //  who throws an exception when they lose to the other player
+    public class SoreLoserUpperHalfPlayer
+    {
+
+    }
+}
